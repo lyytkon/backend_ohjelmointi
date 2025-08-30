d77ceca73c0c406eea7f0a142b8ff44aaa97400a
KonstaLyytikainen <bhq669@myy.haaga-helia.fi>
2025-08-30 15:02:53 +0300

    muutos teht1

--- viikko2/tehtavat/src/main/java/tehtavat/web/HelloController.java
+++ viikko2/tehtavat/src/main/java/tehtavat/web/HelloController.java
@@ -1,6 +1,7 @@
 package tehtavat.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
